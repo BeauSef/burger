a9fc612dc90b86f31b1871121a44ee750ae5dfd2
Beau Hunter <hunter0907@gmail.com>
2019-02-20 20:26:23 -0700

    Making Server Connects

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,11 @@
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE  burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
 	id Int AUTO_INCREMENT NOT NULL,
 	burgerName VARCHAR(255) NOT NULL,
-	devoured boolean DEFAULT true,
+	devoured BOOLEAN DEFAULT false,
 	time TIMESTAMP DEFAULT now() on UPDATE now(),
 	PRIMARY KEY (id) 
     );
