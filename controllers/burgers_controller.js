e29ebf0955b31c29ac9b178ac6cf24dc5de5355f
Beau Hunter <hunter0907@gmail.com>
2019-02-26 17:58:14 -0700

    Fixed Error

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,11 +6,11 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.selectAll(function(data) {
+  burger.all(function(data) {
     var hbsObject = {
       burgers: data
     };
-    // console.log(hbsObject);
+    console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
@@ -28,57 +28,10 @@ router.post("/burgers", function(req, res) {
 router.put("/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-//   console.log("condition", condition);
-
-  burger.update({
-    devoured: true
-  }, condition, function(data) {
-   res.redirect("/");
-  });
-})
-
-
-// Export routes for server.js to use.
-module.exports = router;
-
-
-
-var express = require("express");
-
-var router = express.Router();
-
-// Import the model (cat.js) to use its database functions.
-var burger = require("../models/burger");
-
-// Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  burger.all(function(data) {
-    var hbsObject = {
-      burgers: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
-});
-
-router.post("/api/burgers", function(req, res) {
-  burger.create([
-    "burger_name"
-  ], [
-    req.body.burger_name
-  ], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
-  });
-});
-
-router.put("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
   console.log("condition", condition);
 
   burger.update({
-    burger: req.body.burger_name
+    devoured: true
   }, condition, function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
